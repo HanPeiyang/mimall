2c46ef3fda13b94e8b9c726435da748710a7469b
peiyang.han <1145047174@qq.com>
2020-03-17 15:15:12 +0800

    remove children routing's '/'

--- src/router/index.js
+++ src/router/index.js
@@ -21,17 +21,17 @@ const routes = [
     redirect:'/index',
     children:[
       {
-        path:'/index',
+        path:'index',
         name:'index',
         component:Index
       },
       {
-        path:'/product/:id',
+        path:'product/:id',
         name:'product',
         component:Product
       },
       {
-        path:'/detail/:id',
+        path:'detail/:id',
         name:'detail',
         component:Detail
       }
